fcc10345ad81891e8fdf8328343f4be71f690c0c
maewakka <chlguddn7@gmail.com>
2022-01-17 23:42:09 +0900

    가즈아~

---
+++ src/main/java/hongbro/everytime/EverytimeApplication.java
@@ -0,0 +1,13 @@
+package hongbro.everytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverytimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverytimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongbro/everytime/EverytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package hongbro.everytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverytimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
